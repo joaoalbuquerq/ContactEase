6ecc14e030240807eb5d1efbe58036a25e24be17
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2025-02-01 18:36:06 -0300

    first commit

---
+++ src/main/java/com/l2code/ContactEase/ContactEaseApplication.java
@@ -0,0 +1,13 @@
+package com.l2code.ContactEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2code/ContactEase/ContactEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2code.ContactEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
